6538c8f8b3707a0efe4f5030dcb2c0cc54eee76f
Pinkeycrea <soumumo@gmail.com>
2019-09-30 14:37:18 +0900

    スクロールまで

--- design_cafe/index.js
+++ design_cafe/index.js
@@ -43,3 +43,16 @@ startTimer();
 /*オプションを足す場合はここへ記載*/
 
 });
+
+jQuery(function(){
+    jQuery(window).scroll(function (){
+        jQuery('.fadein').each(function(){
+            var elemPos = jQuery(this).offset().top;
+            var scroll = jQuery(window).scrollTop();
+            var windowHeight = jQuery(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                jQuery(this).addClass('.scrollin');
+            }
+        });
+    });
+  });
